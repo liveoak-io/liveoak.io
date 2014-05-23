5217bdc279abb64abc14fc42f1dc7a3b509e550d
Bob McWhirter <bob@mcwhirter.org>
2014-05-23 14:44:27 -0400

    Use Ray's partial to manage many metrics, maybe.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,6 +4,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new '/blog'
   extension Awestruct::Extensions::Paginator.new :posts, '/blog'
   extension Awestruct::Extensions::Indexifier.new
+  helper Awestruct::Extensions::Partial
   # Indexifier *must* come before Atomizer
   extension Awestruct::Extensions::Atomizer.new :posts, '/blog.atom'
   helper Awestruct::Extensions::GoogleAnalytics
