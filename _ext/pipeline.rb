dc068918540166fc919b7a2fb950c70b01d2517b
Ken Finnigan <ken@kenfinnigan.me>
2014-11-24 22:15:14 -0500

    Initial inclusion of 'Community' content

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,6 @@
 Awestruct::Extensions::Pipeline.new do
   # extension Awestruct::Extensions::Posts.new '/news'
-  # extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
   # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
 end
