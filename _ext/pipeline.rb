b5963625fe64b66505aadd75166c27c84055719f
Ken Finnigan <ken@kenfinnigan.me>
2014-11-26 11:41:05 -0500

    Add blog content

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,8 @@
 Awestruct::Extensions::Pipeline.new do
   helper Awestruct::Extensions::Partial
-  # extension Awestruct::Extensions::Posts.new '/news'
+  extension Awestruct::Extensions::Posts.new '/blog'
+  extension Awestruct::Extensions::Paginator.new :posts, '/blog'
   extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
-  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  extension Awestruct::Extensions::Atomizer.new :posts, '/blog.atom'
 end
