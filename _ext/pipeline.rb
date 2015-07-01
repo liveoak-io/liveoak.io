0cd2fe7692bcb35eae2dbb355fdf3fd41925467d
Ken Finnigan <ken@kenfinnigan.me>
2015-07-01 11:07:51 -0400

    Prevent Indexifier from doing its thing on `javascript_api` folder

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,7 +4,7 @@ Awestruct::Extensions::Pipeline.new do
   helper Awestruct::Extensions::Partial
   extension Awestruct::Extensions::Posts.new '/blog'
   extension Awestruct::Extensions::Paginator.new :posts, '/blog'
-  extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Indexifier.new [/javascript_api\/*/]
   # Indexifier *must* come before Atomizer
   extension Awestruct::Extensions::Atomizer.new :posts, '/blog.atom', :template => '_template.atom.haml'
 end
